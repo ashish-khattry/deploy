b40c4a826dae9996e2a6fa064234392efd546089
Ashish <ashishkhattry7@gmail.com>
2026-09-11 15:44:48 +0530

    Added 2nd program in chapter 10

--- rust_learning_journey/chapter_10_generic_types_traits_and_lifetimes/src/bin/01_generic_largest_char.rs
+++ rust_learning_journey/chapter_10_generic_types_traits_and_lifetimes/src/bin/01_generic_largest_char.rs
@@ -11,5 +11,15 @@ fn largest<T: std::cmp::PartialOrd + Copy>(list: &[T]) -> T {
 fn main() {
     let l1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let large = largest(&l1);
-    println!("Largest={large}")
+    for item in l1 {
+        print!("{}  ", item);
+    }
+    println!("Largest={large}");
+
+    let l2 = vec!['a', 'b', 'e', 's', 'q', 'o', 'x'];
+    let large = largest(&l2);
+    for item in l2 {
+        print!("{}  ", item);
+    }
+    println!("Largest={large}");
 }
