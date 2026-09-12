049335756cc4cd031764b58ce96459108bbfaf8f
Ashish <ashishkhattry7@gmail.com>
2026-09-12 15:39:30 +0530

    Updated 9th  program in chapter 10

--- rust_learning_journey/chapter_10_generic_types_traits_and_lifetimes/src/bin/09_struct_with_lifetime.rs
+++ rust_learning_journey/chapter_10_generic_types_traits_and_lifetimes/src/bin/09_struct_with_lifetime.rs
@@ -1,10 +1,9 @@
 struct ImportantExcerpt<'a> {
-    part: &'a String,
+    part: &'a str,
 }
 fn main() {
     let novel = String::from("Rust is hard, But ashish is CTO.");
     let first_sentense = novel.split(",").next().unwrap();
-    let first_sentense = &first_sentense.to_string();
     let i = ImportantExcerpt {
         part: &first_sentense,
     };
